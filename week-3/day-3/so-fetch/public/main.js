03c1275fb6831e2311ef5eb91f5d865be60424f3
Mark Dewey <mark.t.dewey@gmail.com>
2018-03-21 11:16:21 -0400

    API example

--- week-3/day-3/so-fetch/public/main.js
+++ week-3/day-3/so-fetch/public/main.js
@@ -1,15 +1,25 @@
 const main = () => {
   const lukeUrl = "https://swapi.co/api/people/1/";
- 
+
   console.log("starting request");
   fetch(lukeUrl).then((response) => {
- 
-    console.log("response came back")
+    console.log("response came back", response);
+    if (response.status === 200) {
+      return response.json()
+    }
+  }).then((luke) => {
+    console.log(luke);
+    const output = document.querySelector(".luke-data")
+    output.textContent = `${luke.name} has ${luke.hair_color} hair`;
+    output.textContent = luke.name+" has "+ luke.hair_color + "hair";
+    
   })
- 
+
   console.log('fired request');
 
-  document.querySelector('h1').textContent += '?'
+  document
+    .querySelector('h1')
+    .textContent += '?'
 }
 
 document.addEventListener('DOMContentLoaded', main)
