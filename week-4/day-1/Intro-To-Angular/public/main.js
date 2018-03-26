6b772abf4d0491e844e58fe4843990994766303d
Mark Dewey <mark.t.dewey@gmail.com>
2018-03-26 10:59:20 -0400

    basic example

--- week-4/day-1/Intro-To-Angular/public/main.js
+++ week-4/day-1/Intro-To-Angular/public/main.js
@@ -17,12 +17,29 @@ angular.
       $scope.displayName = $scope.name + " " + getRandomNumber();
     }
 
+
+    $scope.numbers = [1,2,3,4,5]
+    $scope.addRandomNumber = () => {
+      $scope.numbers.push(getRandomNumber());
+    }
+
+    $scope.update3Value = () => {
+      $scope.numbers[2] +=  3
+    }
+    $scope.addOneToThisValue = (index) => {
+      $scope.numbers[index] += 1;
+    }
+
+
+    
+
+
     // $scope.searchValue = "This should go into my search box";
     // console.log($scope.searchValue);
     console.log($scope);
   })
   .controller("otherController", ($scope) =>{
-    
+
   });
 
 
