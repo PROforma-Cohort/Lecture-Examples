aa3fcd3c3186da2cc340d137f3656050ffda1d76
Mark Dewey <mark.t.dewey@gmail.com>
2018-04-03 15:30:15 -0400

    added all 6 categories

---
+++ week-5/day-1/angular-review/C-SHARP.md
@@ -0,0 +1,13 @@
+C#
+
+- Object Oriented Language 
+- Built by Microsoft , because Java wasnt good enough
+- JavaScript Interpreted by the browser
+- C# -> compiled into machine optimized format -> run by the machine
+    - Compile Errors
+        - type checking
+    - Runtime Errors
+    - Logic Errors
+- JavaScript Dynamic Typing
+- C# Static Typing
+    - once we declare a varible, we give a type, and that type cannot change
--- week-5/day-1/angular-review/public/main.js
+++ week-5/day-1/angular-review/public/main.js
@@ -1,5 +1,5 @@
 const app = angular.module("main", []);
-const CATEGORY_API = "http://jservice.io/api/category?id=3228"
+const CATEGORY_API = "http://jservice.io/api/category?id="
 
 app.controller("mainController", [
   "$scope",
@@ -8,39 +8,52 @@ app.controller("mainController", [
 
     $scope.title = "Hello world!!!"
     $scope.player = {
-      score:0
+      score: 0
     }
-
-    $http({method: "GET", url: CATEGORY_API}).then(response => {
-      console.log("back from API,", response.data);
-      // category title category clues (values & questions)
-      // 5 quetsions, 
-        // 100
-        // 200
-        // 300
-        // 400
-        // 500
-      $scope.category = {
-        title: response.data.title,
-        clues: response.data.clues.slice(0, 5)
-      }
-      $scope.category.clues = $scope.category.clues.map((clue, index) => {
-        clue.value = (index  + 1) * 100;
-        return clue;
+    $scope.game = {
+      categories: []
+    }
+    const getCategory = (id) => {
+      $http({
+        method: "GET",
+        url: CATEGORY_API + id
+      }).then(response => {
+        console.log("back from API,", response.data);
+        const _category = {
+          title: response.data.title,
+          clues: response
+            .data
+            .clues
+            .slice(0, 5)
+        }
+        _category.clues = _category
+          .clues
+          .map((clue, index) => {
+            clue.value = (index + 1) * 100;
+            return clue;
+          })
+        console.log(_category)
+        $scope.game.categories.push(_category);
+        console.log($scope.game);
       })
-      console.log($scope.category)
-    })
 
+    }
+    
     $scope.questionSelected = (clue) => {
       console.log("asking", clue);
       $scope.questionWasSelected = true;
-      $scope.selectedQuestion = clue; 
+      $scope.selectedQuestion = clue;
     }
 
     $scope.checkAnswer = () => {
       // compare, userAnswer with selectedQuestionAnswer
-      const isCorrect = $scope.userAnswer.toLowerCase() === $scope.selectedQuestion.answer.toLowerCase()
-      if (isCorrect){
+      const isCorrect = $scope
+        .userAnswer
+        .toLowerCase() === $scope
+        .selectedQuestion
+        .answer
+        .toLowerCase()
+      if (isCorrect) {
         // increase player score by current value
         $scope.player.score += parseInt($scope.selectedQuestion.value)
       } else {
@@ -50,5 +63,13 @@ app.controller("mainController", [
       $scope.questionWasSelected = false;
       $scope.selectedQuestion.answered = true;
     }
+    const init = () => {
+      const _categories = [1,2,3,4,5,6]
+      _categories.forEach(id => {
+        getCategory(id);
+      })
+    }
+    init();
+   
   }
 ])
