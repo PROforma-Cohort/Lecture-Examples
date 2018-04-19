b376e4a0a3adc446c0a2f4ef749a4577533b29f1
Mark Dewey <mark.t.dewey@gmail.com>
2018-04-19 14:23:48 -0400

    PRRRRRRROOF

---
+++ week-7/day-4/ShoeShopping/ShoeAPI/Controllers/ShoppingCartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShoeAPI.Controllers
+{
+    public class ShoppingCartViewModel
+    {
+        public int ShoeId { get; set; }
+        public int Quantity { get; set; }
+    }
+
+
+    public class ShoppingCartController : ApiController
+    {
+        [HttpPost]
+        [Route("api/shoppingcart/{userId}")]
+        public IHttpActionResult AddShoeToShopping([FromUri] int userId, [FromBody]ShoppingCartViewModel data)
+        {
+
+            return Ok();
+        }
+    }
+}
