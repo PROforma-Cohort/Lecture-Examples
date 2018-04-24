7db14000caee84700ace30d3ebac5556e0bda572
Mark Dewey <mark.t.dewey@gmail.com>
2018-04-24 10:14:05 -0400

    added Search controller, not tested et

---
+++ week-8/AngularAndWebAPIReview/WebApplication1/WebApplication1/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Context;
+
+namespace WebApplication1.Controllers
+{
+    public class SearchController : ApiController
+    {
+
+        public IHttpActionResult Get(string title)
+        {
+            var db = new LibraryContext();
+            var results = db.Books.Where(w => title.Contains(w.Title)).ToList();
+            if (results.Count == 0)
+            {
+                return NotFound();
+            } else
+            {            
+                return Ok(results);
+            }
+        }
+    }
+}
