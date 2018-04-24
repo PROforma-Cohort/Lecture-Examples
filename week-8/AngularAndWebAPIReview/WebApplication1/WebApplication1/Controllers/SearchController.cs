bfa374023b9c40ce0068587eb822f5f8cffd9551
Mark Dewey <mark.t.dewey@gmail.com>
2018-04-24 10:43:00 -0400

    done with search controller

--- week-8/AngularAndWebAPIReview/WebApplication1/WebApplication1/Controllers/SearchController.cs
+++ week-8/AngularAndWebAPIReview/WebApplication1/WebApplication1/Controllers/SearchController.cs
@@ -5,18 +5,33 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using WebApplication1.Context;
+using System.Data.Entity;
 
 namespace WebApplication1.Controllers
 {
     public class SearchController : ApiController
     {
-
-        public IHttpActionResult Get(string title)
+        public IHttpActionResult Get(string title = null, string genre = null, string author = null)
         {
             try
             {
                 var db = new LibraryContext();
-                var results = db.Books.Where(w => w.Title.Contains(title)).ToList();
+                // if title == null return all books
+                var query = db.Books.Include(i => i.Author).Include(i => i.Genre);
+                if (!String.IsNullOrEmpty(title))
+                {
+                    query = query.Where(w => w.Title.Contains(title));
+                }
+
+                if (!String.IsNullOrEmpty(genre))
+                {
+                    query = query.Where(w => w.Genre.Name.Contains(genre));
+                }
+                if (!String.IsNullOrEmpty(author))
+                {
+                    query = query.Where(w => w.Author.FullName.Contains(author));
+                }
+                var results = query.ToList();
                 if (results.Count == 0)
                 {
                     return NotFound();
