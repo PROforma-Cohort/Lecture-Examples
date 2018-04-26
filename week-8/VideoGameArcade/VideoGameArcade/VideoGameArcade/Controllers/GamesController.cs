3761090aa074c63252d291c96a43976e59816bd1
Mark Dewey <mark.t.dewey@gmail.com>
2018-04-26 10:52:38 -0400

    game controller'

---
+++ week-8/VideoGameArcade/VideoGameArcade/VideoGameArcade/Controllers/GamesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VideoGameArcade.DataContext;
+using VideoGameArcade.Models;
+
+namespace VideoGameArcade.Controllers
+{
+    public class GamesController : ApiController
+    {
+        // get all game
+        public IEnumerable<Game> GetAllGames()
+        {
+            using (var db = new ArcadeContext())
+            {
+                return db.ArcadeGames.OrderBy(o => o.Title).ToList();
+            }
+        }
+
+        //get one game
+        public IHttpActionResult GetOneGame(int id)
+        {
+            using (var db = new ArcadeContext())
+            {
+                var game = db.ArcadeGames.SingleOrDefault(s => s.Id == id);
+                if (game == null)
+                {
+                    return NotFound();
+                } else
+                {
+                    return Ok(game);
+                }
+            }
+        }
+    }
+}
